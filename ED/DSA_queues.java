61d64dd59d85a4b3bcc9c6f51ee55aa89bbeae9a
Andre Luiz <andreluiz.morais19@gmail.com>
2023-02-16 10:44:09 -0300

    first

---
+++ ED/DSA_queues.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class DSA_queues {
+  public static void main(String[] args) {
+
+    // FIFO DSA == First-in-First-out
+
+    // add() = queue, offer()
+    // remove() - dequeue, poll()
+
+    Queue<Integer> queue = new LinkedList<Integer>();
+    queue.offer(1);
+    queue.offer(2);
+    queue.offer(3);
+    queue.offer(4);
+    queue.offer(5);
+    // First-in-First-out
+    
+    queue.poll();// removes 1
+    queue.poll();// removes 2
+    queue.poll();// removes 3
+    queue.poll();// removes 4
+    queue.poll();// removes 5
+  }
+}

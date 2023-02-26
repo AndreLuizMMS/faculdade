cab0b752ab0d72039ea6ce7e86771f4895a7ff93
Andre Luiz <andreluiz.morais19@gmail.com>
2023-02-26 14:43:29 -0300

    continuidade física

--- ED/Lista_p07.java
+++ ED/Lista_p07.java
@@ -1,7 +1,117 @@
+import java.util.*;
+
 public class Lista_p07 {
-  
+  final static int SUCESSO = 0;
+  final static int LISTA_CHEIA = 1;
+  final static int LISTA_VAZIA = 2;
+
+  // -------------------------------------------------- criaLista
+  static void create(int[] nEle) {
+    nEle[0] = 0;
+  }
+
+  // -------------------------------------------------- adiciona no fim
+  static int push(int vLength, int[] v, int valor) {
+    int n = v[0]; // n° de elementos
+    if (n < vLength - 1) { // se existe espaço na lista
+      v[0]++;
+      v[v[0]] = valor;
+      return (SUCESSO);
+    }
+    return (LISTA_CHEIA);
+  }
+
+  // -------------------------------------------------- adiciona no inicio
+  static int unshift(int vLength, int[] v, int valor) {
+    int n = v[0]; // n° de elementos
+    if (n < vLength - 1) { // se existe espaço na lista
+      v[0]++;
+      for (int i = n + 1; i >= 2; i--) {
+        // i = n + 1 -> loop inicia da última posição vazia;
+        // i >= 2 -> 2 é a primeira posição;
+        // i-- -> começa do fim e ta voltando;
+        v[i] = v[i - 1]; // muda cada um de posição pra 1° ficar vazia
+      }
+      v[1] = valor; // valor entra na primeira
+      return (SUCESSO);
+    }
+    return (LISTA_CHEIA);
+  }
+
+  // -------------------------------------------------- adiciona pela posição
+  static int incluiPos(int vLength, int[] v, int valor, int pos) {
+    int n = v[0];
+
+    if (n < vLength - 1) { // se existe espaço na lista
+      v[0]++;
+      if (pos == n) { // se a posição é a mesma do ultimo elemento;
+        v[v[0]] = v[pos]; // último elemento anda uma pra frente;
+        v[pos] = valor; // casa vazia recebe valor;
+      } else {
+        for (int i = n + 1; i >= pos; i--) { // mesma lógica do unshift
+          v[i] = v[i - 1];
+          // System.out.println("Valor: " + v[i]);
+        }
+        v[pos] = valor;
+      }
+      return (SUCESSO);
+    }
+    return (LISTA_CHEIA);
+  }
+
+  // -------------------------------------------------- numeroDeElementos
+  static int numeroDeElementos(int[] v) {
+    return (v[0]);
+  }
+
+  // ------------------------------------- insere na posição correta de ordenação
+  static int verificaPos(int m, int[] v, int valor) {
+
+    for (int i = 1; i <= v[0]; i++) {
+      if (valor < v[i]) {
+        return (incluiPos(m, v, valor, i));
+      }
+    }
+    return (push(m, v, valor));
+  }
+
+  // -------------------------------------------------- exibeLista
+  static void exibeLista(int[] v) {
+
+    if (v[0] == 0) {
+      System.out.println("Lista Vazia");
+    } else {
+      String s = "";
+      for (int i = 1; i <= v[0]; i++) {
+        s = s + v[i] + " ";
+      }
+      System.out.println("Lista: " + s);
+    }
+  }
+
   public static void main(String[] args) {
-    
-    
+    try (Scanner sc = new Scanner(System.in)) {
+      int m = 7;
+      int[] v = new int[m];
+
+      int erro;
+      create(v);
+      int valor = sc.nextInt();
+      if (valor != 0) {
+        erro = unshift(m, v, valor);
+        while (valor != 0) {
+          valor = sc.nextInt();
+          if (valor != 0) {
+            erro = verificaPos(m, v, valor);
+            if (erro == LISTA_CHEIA) {
+              System.out.println("lista Cheia ");
+              exibeLista(v);
+              return;
+            }
+          }
+        }
+      }
+      exibeLista(v);
+    }
   }
 }
---
+++ ED/Lista_p09.java
@@ -0,0 +1,193 @@
+import java.util.*;
+
+public class Lista_p09 {
+  final static int SUCESSO = 0;
+  final static int LISTA_CHEIA = 1;
+  final static int LISTA_CHEIA_direita = 11;
+  final static int LISTA_CHEIA_esquerda = -11;
+  final static int LISTA_VAZIA = 2;
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    final int m = 4;
+    int[] v = new int[m + 2];
+
+    int valor;
+
+    boolean dirVazio = true;
+    boolean esqVazio = true;
+    int ch;
+
+    criaLista(v);
+
+    int erro = SUCESSO;
+    do {
+      exibeLista(v);
+
+      System.out.println(" Valor: ");
+      valor = sc.nextInt();
+
+      if (valor != 0) {
+        System.out.println(" 0 = Início | 1 = Fim ");
+        ch = sc.nextInt();
+
+        if (ch == 0 && esqVazio) {
+          erro = incluiInicio(v, valor);
+        }
+        if (ch == 1 && dirVazio) {
+          erro = incluiFim(v, valor);
+        }
+
+      }
+
+      imprimeErro(erro);
+
+      if (!dirVazio && !esqVazio) {
+        System.out.println();
+        System.out.println("LISTA COMPLETAMENTE CHEIA");
+        break;
+      }
+      if (erro == LISTA_CHEIA_direita) {
+        dirVazio = false;
+      }
+      if (erro == LISTA_CHEIA_esquerda) {
+        esqVazio = false;
+      }
+
+    } while (valor != 0 && erro != LISTA_CHEIA);
+
+    exibeLista(v);
+
+  }
+
+  // ---------------------------------------------- criaLista
+  static void criaLista(int[] v) {
+    int inic = 0;
+    int fim = v.length - 1; // index da última casa
+
+    // -1 nas extremidades == lista vazia
+    // [inic] 0 , [fim] m - 1 == lista cheia / existe algum elemento
+    v[inic] = -1;
+    v[fim] = -1;
+  }
+
+  // ---------------------------------------------- incluiInicio
+  static int incluiInicio(int[] v, int dado) {
+    int inic = v[0];
+    int fim = v[v.length - 1];
+
+    // index do último elemento
+    int m = v.length - 2;
+
+    // index do elemento do meio
+    int metade = (m / 2) + 1;
+
+    if (inic == -1) {
+      // se a lista estiver vazia, dado é o 1° a entrar;
+      // inserido sempre no meio
+
+      // apontam pra casa onde dado entra
+      inic = metade;
+      fim = inic;
+
+      // v[no meio] = dado
+      v[inic] = dado;
+
+      // v[0] aponta o index do início da lista
+      v[0] = inic;
+      // v[última casa] aponta o index do fim da lista
+      v[v.length - 1] = fim;
+
+      return (SUCESSO);
+
+    }
+
+    if (v[0] == 1) { // 1° casa ocupada
+      return (LISTA_CHEIA_esquerda);
+    }
+
+    inic--; // inicio marca um a menos;
+    v[inic] = dado; // dado entra na primeira casa;
+    v[0] = inic; // v[0] aponta o inicio da lista;
+    return (SUCESSO);
+
+  }
+
+  // ---------------------------------------------- incluiFim
+  static int incluiFim(int[] v, int dado) {
+    int inic = v[0];
+    int fim = v[v.length - 1];
+
+    int m = v.length - 2;
+
+    int metade = (m / 2) + 1;
+
+    if (fim == -1) {
+      // se a lista estiver vazia, dado é o 1° a entrar;
+      // inserido sempre no meio
+
+      // apontam pra casa onde dado entra
+      inic = metade;
+      fim = inic;
+
+      // v[no meio] = dado
+      v[inic] = dado;
+
+      // v[0] aponta o index do início da lista
+      v[0] = inic;
+      // v[última casa] aponta o index do fim da lista
+      v[v.length - 1] = fim;
+
+      return (SUCESSO);
+    }
+
+    if (fim == m) { // fim aponta pro último elemento
+      return (LISTA_CHEIA_direita);
+    }
+
+    fim++;
+    v[fim] = dado;
+    v[v.length - 1] = fim;
+
+    return (SUCESSO);
+  }
+
+  // ---------------------------------------------- imprimeErro
+  static void imprimeErro(int erro) {
+    switch (erro) {
+      case LISTA_VAZIA:
+        System.out.println("ERRO: Lista Vazia");
+        break;
+      case LISTA_CHEIA_direita:
+        System.out.println("ERRO: Lista cheia na direita");
+        break;
+      case LISTA_CHEIA_esquerda:
+        System.out.println("ERRO: Lista cheia na esquerda");
+        break;
+      case LISTA_CHEIA:
+        System.out.println("ERRO: Lista cheia ");
+        break;
+
+    }
+  }
+
+  // ---------------------------------------------- exibeLista
+  static void exibeLista(int[] v) {
+    int inic = v[0];
+    int fim = v[v.length - 1];
+
+    System.out.println("inic: " + inic);
+    System.out.println(" fim: " + fim);
+    System.out.print("indices: ");
+    for (int i = 0; i < v.length; i++) {
+      System.out.print(" " + i);
+    }
+    System.out.println();
+    System.out.print("Lista:   ");
+    for (int i = 0; i < v.length; i++) {
+      System.out.print(" " + v[i]);
+    }
+    System.out.println();
+
+  }
+}
